1e8e9f4d507de594758fcf51b34625cf32db9df1
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2025-05-30 15:22:04 -0600

    Add route that gets access token given the code

--- src/main/java/com/spotifyproject/yourspotify/controller/Controller.java
+++ src/main/java/com/spotifyproject/yourspotify/controller/Controller.java
@@ -1,6 +1,12 @@
 package com.spotifyproject.yourspotify.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,9 +26,30 @@ public class Controller {
     @Value("${spotify.redirect-uri}")
     private String redirectUri;
 
-
     @GetMapping("/test")
     public String sendResponse() {
         return "valid";
     }
+
+    @PostMapping("/auth/token")
+    public ResponseEntity<?> getAccessToken(@RequestBody Map<String, String> body) {
+
+        String tokenEndpoint = "https://accounts.spotify.com/api/token";
+        String code = body.get("code");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.setBasicAuth(clientId, clientSecret);
+
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+        form.add("grant_type", "authorization_code");
+        form.add("code", code);
+        form.add("redirect_uri", redirectUri);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(form, headers);
+        ResponseEntity<Map> response = restTemplate.postForEntity(tokenEndpoint, request, Map.class);
+
+        return ResponseEntity.ok(response.getBody());
+    }
+
 }
