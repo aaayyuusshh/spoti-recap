4c5cec97f1c89b7a30e9b5877f03b7fbb8b56947
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2025-06-29 16:12:33 -0600

    Add a global exception handler for missing headers

--- src/main/java/com/spotifyproject/yourspotify/advice/GlobalExceptionHandler.java
+++ src/main/java/com/spotifyproject/yourspotify/advice/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.spotifyproject.yourspotify.advice;
 
 import org.springframework.http.*;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 
@@ -14,6 +15,16 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .badRequest()
                 .body(Map.of("error", "Missing or invalid request body"));
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<?> handleMissingRequestHeader(MissingRequestHeaderException ex) {
+        String headerName = ex.getHeaderName();
+        String errorMsg = "Missing required header: " + headerName;
+        HttpStatus status = "Authorization".equalsIgnoreCase(headerName) ? HttpStatus.UNAUTHORIZED : HttpStatus.BAD_REQUEST;
+        return ResponseEntity
+                .status(status)
+                .body(Map.of("error", errorMsg));
     }
 
     // triggered for uncaught exceptions
