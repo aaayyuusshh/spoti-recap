4a48677ec88b54af23c4f64f87ae82f13568b905
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2025-07-05 01:47:41 -0600

    Create a custom spotify bad gateway exception

---
+++ src/main/java/com/spotifyproject/yourspotify/exception/SpotifyApiException.java
@@ -0,0 +1,8 @@
+package com.spotifyproject.yourspotify.exception;
+
+public class SpotifyApiException extends RuntimeException {
+
+    public SpotifyApiException(String message) {
+        super(message);
+    }
+}
