eea4e99c5322da00dd01c167746a6d290237249f
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2025-07-18 03:09:21 -0600

    Add redis config

---
+++ backend/src/main/java/com/spotifyproject/spotirecap/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.spotifyproject.spotirecap;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.password:}")
+    private String password;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+
+        if (password != null && !password.isBlank()) {
+            config.setPassword(password);
+        }
+
+        return new LettuceConnectionFactory(config);
+    }
+}
